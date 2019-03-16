a220b8d556e7bdf2f3d1cf3cf716d54f7f1f3d8c
Jason Xue <jasonxue1228@gmail.com>
2019-03-16 10:57:48 -0600

    Setup private route

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import 'react-dates/lib/css/_datepicker.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { startSetExpenses } from './actions/expenses';
+import { login, logout} from './actions/auth';
 import './firebase/firebase';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import './styles/styles.scss';
+import { firebase } from './firebase/firebase'
 // import './playground/promises'
 
 const store = configureStore();
@@ -18,9 +20,28 @@ const jsx = (
   </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
+
 ReactDOM.render(<p>loading......</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid))
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/' ) {
+        history.push('/dashboard');
+      }
+    })
+  } else {
+    store.dispatch(logout())
+    renderApp();
+    history.push('/');
+  }
 })
-
